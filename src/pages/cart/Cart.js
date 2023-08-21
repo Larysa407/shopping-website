70fe7fda832fbb18abc42396e49a38171e694e8d
Larysa Semerikova <lorik280319@gmail.com>
2023-08-21 10:59:41 +0300

    localStorage left

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -7,25 +7,27 @@ export const Cart = () => {
   const cart = useSelector((state) => state.cart);
   return (
     <div className="wrapper">
-      <div className="form-container">
-      <Forms/>
-      </div>
-      <div className="cart-product-list">
-        {cart.length !== 0 ? (
-          cart.map((cartItem) => (
-            <CartShow
-              title={cartItem.title}
-              price={cartItem.price}
-              image={cartItem.image}
-              color={cartItem.color}
-              id={cartItem.id}
-              key={cartItem.id}
-            />
-          ))
-        ) : (
-          <p className="empty-cart">The cart is empty</p>
-        )}
-      </div>
-      </div>
+      {cart.length !== 0 ? (
+        <>
+          <div className="form-container">
+            <Forms />
+          </div>
+          <div className="cart-product-list">
+            {cart.map((cartItem) => (
+              <CartShow
+                title={cartItem.title}
+                price={cartItem.price}
+                image={cartItem.image}
+                color={cartItem.color}
+                id={cartItem.id}
+                key={cartItem.id}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="empty-cart">The cart is empty</p>
+      )}
+    </div>
   );
 };
